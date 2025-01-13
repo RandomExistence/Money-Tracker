4ce96377dbc7f42629efe7eff22245ebbdec109e
Parit Siriphuwanich <phroggothesecond@gmail.com>
2025-01-13 22:26:15 +0700

    create updateProductHistory and toggleHistoryExpand. update renderHistory to relates more to the productHistory array. Implement localStorage for productHistory

--- script/history.js
+++ script/history.js
@@ -1,7 +1,17 @@
-export let productHistory = [
+import { formatMonth } from "./utils/dateFormat.js";
+import { productToday ,  dailyEarn } from "./spending.js"
+
+let productHistory = JSON.parse(localStorage.getItem('productHistory')) || [
+  {
+    date: 'Jan 2025',
+    totalPrice: 0,
+    maxPrice: 0,
+    thisMonthHistory: []
+  },
   {
     date: 'Dec 2024',
     totalPrice: 30,
+    maxPrice: 100,
     thisMonthHistory: [
       // would be nice if we could make it a class, you know to make it more concise
       {
@@ -16,7 +26,8 @@ export let productHistory = [
   },
   {
     date: 'Nov 2024',
-    totalPrice: 30,
+    totalPrice: 40,
+    maxPrice: 120,
     thisMonthHistory: [
       // would be nice if we could make it a class, you know to make it more concise
       {
@@ -24,8 +35,8 @@ export let productHistory = [
         price: 20
       },
       {
-        name: 'eraser',
-        price: 10
+        name: 'pencil',
+        price: 20
       }
     ]
   },
@@ -38,23 +49,66 @@ document.querySelector('.js-return-button').addEventListener('click', () => {
 function renderHistory() {
   let historyMainHTML = '';
 
-  for (let i = 0; i < 12; ++i) {
+  productHistory.forEach((monthHistory, index) => {
     historyMainHTML += `
-      <button class="expand-button js-expand-button" data-index="${i}">expand</button>
-      <div class="record-date">Dec 2024</div>
-      <div class="record-value">2345/3500</div>
+      <button class="expand-button js-expand-button" data-index="${index}">expand</button>
+      <div class="record-date">${monthHistory.date}</div>
+      <div class="record-value">${monthHistory.totalPrice} / ${monthHistory.maxPrice}</div>
+      <div class="expand-month-history hidden js-expand-month-history-${index}" data-rendered="false"></div>
       <br>
     `;
-  }
+  });
   document.querySelector('.js-main-history-body-div').innerHTML = historyMainHTML;
 
   document.querySelectorAll('.js-expand-button').forEach((button) => {
     button.addEventListener('click', () => {
-      console.log('expanded');
-      console.log(button.dataset.index);
+      // expand the history
+      toggleHistoryExpand(Number(button.dataset.index));
     });
 
   });
+}
+
+// not new month, the first element is off our concern
+export function updateProductHistory() {
+  let yesterdayTotal = 0;
+  productToday.forEach((product) => {
+    productHistory[0].thisMonthHistory.add({
+      name: product.name,
+      price: product.price
+    });
+    yesterdayTotal += product.price;
+  });
+  productHistory[0].totalPrice += yesterdayTotal;
+  productHistory[0].maxPrice += dailyEarn;
+  localStorage.setItem('productHistory', JSON.stringify(productHistory));
+}
+
+// new month
+export function appendProductHistory() {
+
+}
+
+function toggleHistoryExpand(index) {
+  const thisGrid =  document.querySelector(`.js-expand-month-history-${index}`);
+  const isRendered = thisGrid.dataset.rendered;
+  console.log(isRendered);
+  if (!thisGrid.classList.contains('hidden')) {
+    thisGrid.classList.add('hidden');
+    return;
+  }
+  if (isRendered == "false") {
+    let gridHistory = ``;
+    productHistory[index].thisMonthHistory.forEach((product) => {
+      gridHistory += `
+        <div>${product.name} ${product.price}</div>
+      `;
+    });
+    console.log("render");
+    thisGrid.innerHTML = gridHistory;
+    thisGrid.dataset.rendered = "true";
+  }
+  thisGrid.classList.remove('hidden');
 }
 
 renderHistory();
